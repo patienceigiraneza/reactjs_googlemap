b3678195072800078164d86aeb8c838a36c82424
patienceigiraneza <patienceigiraneza2@gmail.com>
2024-05-01 16:48:49 +0200

    texts

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="bg-gradient-to-br from-teal-200 to-lime-200 py-4 h-14 capitalize flex justify-between items-center text-xl">
       <ul className="flex ml-4">
-        <li className="px-4"> Track </li>
+        <li className="px-4"> Home </li>
         <li className="px-4"> About </li>
       </ul>
 
